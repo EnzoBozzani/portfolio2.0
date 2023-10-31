06bf9ea6b8b18d5185348d835514b064b87daa65
Enzo Bozzani <bozzanienzo@gmail.com>
2023-10-30 22:22:17 -0300

    feat: continued developing experience card

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,7 +1,31 @@
 export const ExperienceCard: React.FC = () => {
     return (
-        <article className='max-w-screen-lg border-2 rounded border-gray px-4'>
-            (algo similar ao linkedin sei la)
-        </article>
+        <div className='max-w-screen-xl border-2 border-solid rounded border-gray px-4 flex flex-col'>
+            <div className="w-full flex justify-between">
+                <span>
+                    FrontEnd Developer at CADFEI: Professor Career Institucional Website
+                </span>
+                <div className='flex justify-center items-center'>
+                    <svg
+                        className="w-24 transition duration-500"
+                        xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="26px" height="26px" fillRule="nonzero"><g fill='#ffffff5e' fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                            <g transform="scale(5.33333,5.33333)"><path d="M24,34c-12.9,0 -23,-4.4 -23,-10c0,-5.6 10.1,-10 23,-10c12.9,0 23,4.4 23,10c0,5.6 -10.1,10 -23,10zM24,16c-12.6,0 -21,4.1 -21,8c0,3.9 8.4,8 21,8c12.6,0 21,-4.1 21,-8c0,-3.9 -8.4,-8 -21,-8z"></path><path d="M15.1,44.6c-1,0 -1.8,-0.2 -2.6,-0.7c-4.9,-2.8 -3.6,-13.7 2.8,-24.9v0c3,-5.2 6.7,-9.6 10.3,-12.4c3.9,-3 7.4,-3.9 9.8,-2.5c2.5,1.4 3.4,4.9 2.8,9.8c-0.6,4.6 -2.6,10 -5.6,15.2c-3,5.2 -6.7,9.6 -10.3,12.4c-2.6,2 -5.1,3.1 -7.2,3.1zM32.9,5.4c-1.6,0 -3.7,0.9 -6,2.7c-3.4,2.7 -6.9,6.9 -9.8,11.9v0c-6.3,10.9 -6.9,20.3 -3.6,22.2c1.7,1 4.5,0.1 7.6,-2.3c3.4,-2.7 6.9,-6.9 9.8,-11.9c2.9,-5 4.8,-10.1 5.4,-14.4c0.5,-4 -0.1,-6.8 -1.8,-7.8c-0.5,-0.2 -1,-0.4 -1.6,-0.4z"></path><path d="M33,44.6c-5,0 -12.2,-6.1 -17.6,-15.6c-6.5,-11.2 -7.8,-22.1 -2.9,-24.9v0c4.9,-2.8 13.7,3.7 20.2,14.9c3,5.2 5,10.6 5.6,15.2c0.7,4.9 -0.3,8.3 -2.8,9.8c-0.8,0.4 -1.6,0.6 -2.5,0.6zM13.5,5.8c-3.3,1.9 -2.7,11.3 3.6,22.2c6.3,10.9 14.1,16.1 17.4,14.2c1.7,-1 2.3,-3.8 1.8,-7.8c-0.6,-4.3 -2.5,-9.4 -5.4,-14.4c-6.3,-10.9 -14.1,-16.1 -17.4,-14.2z"></path><circle cx="24" cy="24" r="4"></circle></g>
+                        </g>
+                    </svg>
+                    <svg
+                        className="w-24 transition duration-500"
+                        xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="26px" height="26px" fillRule="nonzero"><g fill='#ffffff5e' fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                            <g transform="scale(5.12,5.12)"><path d="M45,4h-40c-0.553,0 -1,0.448 -1,1v40c0,0.552 0.447,1 1,1h40c0.553,0 1,-0.448 1,-1v-40c0,-0.552 -0.447,-1 -1,-1zM29,26.445h-5v15.555h-4v-15.555h-5v-3.445h14zM30.121,41.112v-4.158c0,0 2.271,1.712 4.996,1.712c2.725,0 2.62,-1.782 2.62,-2.026c0,-2.586 -7.721,-2.586 -7.721,-8.315c0,-7.791 11.25,-4.717 11.25,-4.717l-0.14,3.704c0,0 -1.887,-1.258 -4.018,-1.258c-2.131,0 -2.9,1.013 -2.9,2.096c0,2.795 7.791,2.516 7.791,8.141c0.001,8.664 -11.878,4.821 -11.878,4.821z"></path></g>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div className="w-full text-gray flex justify-center items-center">
+                FrontEnd Development using React, TypeScript, Redux, CSS & Vite.
+            </div>
+        </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const classes = {
-    homeAnchor: 'w-fit py-1.5 px-5 text-white text-xl sm:text-2xl md:text-4xl flex justify-start items-start transition duration-500 hover:scale-125'
+    homeAnchor: 'w-fit py-1.5 px-5 text-white text-xl sm:text-2xl md:text-4xl flex justify-center items-center gap-2 transition duration-500 hover:text-slate-300'
 }
 
 export const Header: React.FC<Props> = ({ setIsOpen }: Props) => {
@@ -18,7 +18,12 @@ export const Header: React.FC<Props> = ({ setIsOpen }: Props) => {
             <>
                 <header className="flex justify-between p-2 md:p-6 w-full mb-8">
                     <Link href={'/'} data-aos='flip-up' className={classes.homeAnchor}>
-                        <span className="text-green me-2">{'</>'}</span><span>Enzo</span><span>Bozzani</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                            className="w-8 h-8 text-green"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                        </svg>
+                        <span>Enzo</span><span>Bozzani</span>
                     </Link>
                     <button data-aos='flip-up' onClick={() => setIsOpen(true)} className='border border-white rounded px-2 hover:border-green'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 text-white">
@@ -33,7 +38,12 @@ export const Header: React.FC<Props> = ({ setIsOpen }: Props) => {
     return (
         <header className="flex justify-between p-6 w-full mb-8">
             <Link data-aos='flip-up' href={'/'} className={classes.homeAnchor}>
-                <span className="text-green me-2">{'</>'}</span><span>Enzo</span><span>Bozzani</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                    className="w-8 h-8 text-green"
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                </svg>
+                <span>Enzo</span><span>Bozzani</span>
             </Link>
             <nav className="flex justify-center items-center gap-2.5 w-fit">
                 <Link data-aos='flip-up' href={'/experience'} className="text-gray transition duration-500 border-b-2 border-dark pb-3 hover:border-green hover:text-white">
