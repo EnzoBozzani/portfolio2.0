e05d618c1bfbd61015716a35c8a77f688b809c4a
Enzo Bozzani <bozzanienzo@gmail.com>
2024-01-26 01:42:31 -0300

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import { Inter } from 'next/font/google';
 import '@/src/styles/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<main>
+					{children}
+					<Analytics />
+				</main>
+			</body>
+		</html>
+	);
 }
